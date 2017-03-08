b38a19c04d43c87c8db7690128803d7f8f5100ba
Alex <alex@DPRK>
2017-03-08 00:49:15 +0000

    started tr data formatter

---
+++ trFormat.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.IOException;
+
+public static class trFormat
+{
+	public static void Main(String[] args)
+	{
+		private static final String file = args[0];
+
+		
+	}
+} 
+
+
+
---
+++ trformat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int 
+main()
+{
+
+}
+
+int
+readfile()
+{
+
+}
+
+int formatout()
+{
+
+}
